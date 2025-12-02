88a2306205771a883366eca43919ca853d0195cb
simon elenbrink <simonelenbrink@gmail.com>
2025-12-02 14:11:17 +0100

    added test to API

---
+++ IntegrationTest/ProductTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTest
+
+{
+    public class ProductTest(ITestOutputHelper output)
+    {
+        private readonly HttpClient _httpClient = new HttpClient()
+        {
+            BaseAddress = new System.Uri("https://fakestoreapi.com/")
+        };
+
+        private readonly ITestOutputHelper _output = output;
+
+        [Fact]
+        public async Task GetProducts()
+        {
+            var result = await _httpClient.GetAsync("products");
+            _output.WriteLine($"Statuskod: {(int)result.StatusCode} ({result.StatusCode})");
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+
+    }
+
+}
